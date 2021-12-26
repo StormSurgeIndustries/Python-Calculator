0b8d8e33aad82a7cacc7f1b5fcec82e1cce9d88a
WestinFrancis <79235234+WestinFrancis@users.noreply.github.com>
2021-12-26 14:05:06 -0700

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,13 +1,17 @@
-import time
 print("Hi! Welcome to the Python Calculator!")
 print("what operation do you want?")
-print("options are: add=a, subtract=s, multiply=m, divide=d.")
+print("options are: add, subtract, multiply, divide.")
+#answer stuff
+m = "m"
+a = "a"
+d = "d"
+s = "s"
 answer = input()
-if answer == 'm':
+if answer == m:
     import multiply.py
-if answer == 'a':
+if answer == a:
     import add.py
-if answer == 'd':
+if answer == d:
     import divide.py
-if answer == 's':
+if answer == s:
     import subtract.py
